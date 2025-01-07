8935da4903ed2af1a917e484ee63439796526fde
Brent Mode <brent.a.mode@gmail.com>
2025-01-07 17:23:42 -0600

    currently working out a skeleton for card data storage and deck list parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 #![allow(unused)]
 
 use std::fmt;
+use std::fs::File;
+use std::io::Write;
+use std::sync::mpsc::{channel, Sender, Receiver};
+
+use nom::{IResult, bytes::complete::tag};
+use serde::{Serialize, Deserialize};
 
 fn main() {
     let franky = Card {
         name: "Franky".to_string(),
         identifier: "OP09-072".to_string(),
-        art: "alt".to_string(),
+        art: "P1".to_string(),
         cost: CardCost(4),
         category: CardCategory::Character,
         power: Some(CardPower(5000)),
@@ -17,11 +23,96 @@ fn main() {
         effects: vec![Effect::Blocker, Effect::TimedEffect(Timing::OnPlay, EffectCost::MinusDon(2), Box::new(Effect::Draw(2)))],
         attached_don: vec![],
     };
+
+    let basic_don = Card {
+        name: "Your Turn +1000".to_string(),
+        identifier: "DON-don".to_string(),
+        art: "P0".to_string(),
+        cost: CardCost(0),
+        category: CardCategory::Don,
+        power: None,
+        counter_power: None,
+        attribute: vec![],
+        color: vec![],
+        types: vec![],
+        effects: vec![Effect::PlusPower(1000)],
+        attached_don: vec![],
+    };
+
+    let franky_serial = serde_json::to_string_pretty(&franky).unwrap();
     
-    println!("{}", franky);
+    let deck = vec![franky.clone(), franky.clone(), franky.clone(), franky.clone()];
+    
+    println!("{:?}", deck);
+
+    let mut file = File::create("assets/card_data/OP09-072-P0.json").unwrap();
+    file.write(franky_serial.as_bytes()).unwrap();
+
 }
 
-#[derive(Debug, Clone, Copy)]
+fn parse_deck_list(deck_list: &str) -> Result<(Card, Vec<Card>, Vec<Card>), DeckParseError> {
+    let leader: Card;
+    let mut main_deck: Vec<Card> = vec![];
+    let mut don_deck: Vec<Card> = vec![];
+
+    struct DeckListEntry {
+        quantity: i32,
+        id: String,
+        name: String,
+        art: String,
+    }
+
+    let mut deck_list_entries: Vec<DeckListEntry> = vec![];
+
+    fn is_comment(line: &str) -> bool {
+        line.starts_with("//")
+    }
+
+    for line in deck_list.lines() {
+        if is_comment(line) {
+            continue;
+        }
+
+        let mut art = String::new();
+
+        let line_contents = line.split_whitespace().collect::<Vec<&str>>();
+        match line_contents.len() {
+            n if n < 3 => return Err(DeckParseError::IncompleteLine(line.to_string())),
+            3 => art = "P0".to_string(),
+            4 => art = line_contents[3].to_string(),
+            n if n > 4 => return Err(DeckParseError::TooManyFields(line.to_string())),
+            _ => unreachable!()
+        };
+
+        let quantity = line_contents[0].parse::<i32>().unwrap();
+        let id = line_contents[1].to_string();
+        let name = line_contents[2].to_string();
+
+        let entry = DeckListEntry {
+            quantity,
+            id,
+            name,
+            art,
+        };
+
+        deck_list_entries.push(entry);
+    }
+
+    todo!();
+}
+
+pub enum DeckParseError {
+    NoLeader,
+    TooManyLeaders,
+    TooManyCopies(i32), // Max number of any particular card id in the main deck is 4 copies.
+    IncompleteLine(String),
+    TooManyFields(String),
+    InvalidCardId(String),
+    InvalidCardName(String),
+    InvalidCardArt(String),
+}
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub enum CardColor {
     Red,
     Blue,
@@ -32,19 +123,19 @@ pub enum CardColor {
 }
 
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub struct CardCost(i32);
 
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub struct CardPower(i32);
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub struct CounterPower(i32);
 
 
  
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub enum CardCategory {
     Leader,
     Character,
@@ -54,7 +145,7 @@ pub enum CardCategory {
 }
 
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub enum Attribute {
     Slash, // SL
     Strike, // ST
@@ -63,7 +154,7 @@ pub enum Attribute {
     Wisdom, // WS
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Card {
     pub name: String,
     pub identifier: String, // Identifies the card outside of the card art, determines uniqueness in a deck list.
@@ -81,7 +172,7 @@ pub struct Card {
 
 pub type Deck = Vec<Card>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Player {
     pub name: String,
     pub leader: Card,
@@ -91,9 +182,21 @@ pub struct Player {
     pub trash: Deck,
 }
 
+pub struct PlayerClient {
+    pub player: Box<Player>,
+    pub tx: Sender<PlayerAction>,
+    pub rx: Receiver<ServerMessage>,
+}
+
 pub struct PlayField {
-    pub player_1: Player,
-    pub player_2: Player,
+    pub player_1: Box<Player>,
+    pub player_2: Box<Player>,
+    pub p1_sender: Sender<ServerMessage>,
+    pub p2_sender: Sender<ServerMessage>,
+    pub p1_receiver: Receiver<PlayerAction>,
+    pub p2_receiver: Receiver<PlayerAction>,
+    pub p1_life_area: Deck,
+    pub p2_life_area: Deck,
     pub p1_stage_area: Deck,
     pub p2_stage_area: Deck,
     pub p1_character_area: Deck,
@@ -111,10 +214,22 @@ pub struct PlayField {
 }
 
 impl PlayField {
-    pub fn setup(player_1: Player, player_2: Player) -> PlayField {
-        PlayField {
-            player_1,
-            player_2,
+    pub fn setup(player_1: Box<Player>, player_2: Box<Player>) -> (PlayField, PlayerClient, PlayerClient) {
+        let (p1_sender, p1_server_receiver) = channel();
+        let (p2_sender, p2_server_receiver) = channel();
+
+        let (p1_client_sender, p1_receiver) = channel();
+        let (p2_client_sender, p2_receiver) = channel();
+
+        (PlayField {
+            player_1: player_1.clone(),
+            player_2: player_2.clone(),
+            p1_sender,
+            p2_sender,
+            p1_receiver,
+            p2_receiver,
+            p1_life_area: Deck::new(),
+            p2_life_area: Deck::new(),
             p1_stage_area: Deck::new(),
             p2_stage_area: Deck::new(),
             p1_character_area: Deck::new(),
@@ -129,11 +244,24 @@ impl PlayField {
             p2_active_don_area: Deck::new(),
             p1_rested_don_area: Deck::new(),
             p2_rested_don_area: Deck::new(),
-        }
+        },
+        PlayerClient {
+            player: player_1.clone(),
+            tx: p1_client_sender,
+            rx: p1_server_receiver,
+        },
+        PlayerClient {
+            player: player_2.clone(),
+            tx: p2_client_sender,
+            rx: p2_server_receiver,
+        })
     }
 }
 
-#[derive(Debug, Clone)]
+pub enum PlayerAction {}
+pub enum ServerMessage {}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum TurnPhase {
     Refresh,
     Draw,
@@ -142,7 +270,7 @@ pub enum TurnPhase {
     End,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum MainPhaseAction {
     PlayCard, 
     ActivateCardEffect,
@@ -152,7 +280,7 @@ pub enum MainPhaseAction {
 
 pub const MAX_CHARACTER_AREA: i32 = 5;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Timing {
     OnPlay,
     WhenAttacking,
@@ -161,17 +289,18 @@ pub enum Timing {
 }
 
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Effect {
     Blocker,
-    TimedEffect(Timing, EffectCost, Box<Effect>),
     Draw(i32),
     OncePerTurn,
+    PlusPower(i32),
     Rush,
+    TimedEffect(Timing, EffectCost, Box<Effect>),
 }
 
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum EffectCost {
     MinusDon(i32),
     RestDon(i32),
@@ -187,12 +316,12 @@ mod display_implementations {
             use CardColor::*;
             let mut val = String::new();
             match self {
-                Red => val = "Red".into(),
-                Blue => val = "Blue".into(),
-                Green => val = "Green".into(),
-                Purple => val = "Purple".into(),
-                Black => val = "Black".into(),
-                Yellow => val = "Yellow".into(),
+                Red => val = "R".into(),
+                Blue => val = "B".into(),
+                Green => val = "G".into(),
+                Purple => val = "P".into(),
+                Black => val = "K".into(),
+                Yellow => val = "Y".into(),
             }
             write!(f, "{val}")?;
             Ok(())
@@ -286,6 +415,7 @@ mod display_implementations {
                 Draw(i) => write!(f, "Draw {}", i)?,
                 OncePerTurn => write!(f, "Once Per Turn")?,
                 Rush => write!(f, "Rush")?,
+                PlusPower(i) => write!(f, "+{i}")?,
             }
             Ok(())
         }
