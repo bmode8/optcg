21e4e44d44a75182d91a4538946f7d44754522a7
Brent Mode <brent.a.mode@gmail.com>
2025-01-07 12:18:34 -0600

    first pass at pretty printing a card.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use std::fmt;
+
 fn main() {
     let franky = Card {
         name: "Franky".to_string(),
@@ -16,7 +18,7 @@ fn main() {
         attached_don: vec![],
     };
     
-    println!("{:?}", franky);
+    println!("{}", franky);
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -29,14 +31,53 @@ pub enum CardColor {
     Yellow,
 }
 
+impl fmt::Display for CardColor {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use CardColor::*;
+        let mut val = String::new();
+        match self {
+            Red => val = "Red".into(),
+            Blue => val = "Blue".into(),
+            Green => val = "Green".into(),
+            Purple => val = "Purple".into(),
+            Black => val = "Black".into(),
+            Yellow => val = "Yellow".into(),
+        }
+        write!(f, "{val}")?;
+        Ok(())
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct CardCost(i32);
+
+impl fmt::Display for CardCost {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)?;
+        Ok(())
+    }
+}
 
 #[derive(Debug, Clone, Copy)]
 pub struct CardPower(i32);
 
+impl fmt::Display for CardPower {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)?;
+        Ok(())
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct CounterPower(i32);
+
+impl fmt::Display for CounterPower {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)?;
+        Ok(())
+    }
+}
+
  
 #[derive(Debug, Clone, Copy)]
 pub enum CardCategory {
@@ -47,15 +88,42 @@ pub enum CardCategory {
     Don,
 }
 
-#[derive(Debug, Clone, Copy)]
-pub enum Attribute {
-    Slash,
-    Strike,
-    Ranged,
-    Special,
-    Wisdom,
+impl fmt::Display for CardCategory {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use CardCategory::*;
+        let mut val = String::new();
+        match self {
+            Leader => val = "Leader".into(),
+            Character => val = "Character".into(),
+            Event => val = "Event".into(),
+            Stage => val = "Stage".into(),
+            Don => val = "DON!! CARD".into(),
+        }
+        Ok(())
+    }
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum Attribute {
+    Slash, // SL
+    Strike, // ST
+    Ranged, // RN
+    Special, // SP
+    Wisdom, // WS
+}
+
+impl fmt::Display for Attribute {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Attribute::Slash => write!(f, "SL")?,
+            Attribute::Strike => write!(f, "ST")?,
+            Attribute::Ranged => write!(f, "RN")?,
+            Attribute::Special => write!(f, "SP")?,
+            Attribute::Wisdom => write!(f, "WS")?,
+        };
+        Ok(())
+    }
+}
 #[derive(Debug, Clone)]
 pub struct Card {
     pub name: String,
@@ -70,6 +138,43 @@ pub struct Card {
     pub types: Vec<String>, // Some cards have more than one type.
     pub effects: Vec<Effect>, 
     pub attached_don: Deck, // Only Leader and Character cards can have a don attached
+}
+
+impl fmt::Display for Card {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "--------------------------------------\n")?;
+        write!(f, "| {}                          {} ", self.cost, self.power.unwrap_or(CardPower(0)))?; 
+        for att in self.attribute.iter() { 
+            write!(f, "{}", att)?;
+        } 
+        write!(f, "  |\n")?;
+        write!(f, "|                                    |\n")?;
+        write!(f, "|                                    |\n")?;
+        write!(f, "|                                    |\n")?;
+        write!(f, "|                                    |\n")?;
+        write!(f, "|  {}                              |\n", self.counter_power.unwrap_or(CounterPower(0)))?;
+        write!(f, "|                                    |\n")?;
+        for effect in self.effects.iter() {
+            write!(f, "|  {}  |\n", effect)?;
+        }
+        for i in 0..(5 - self.effects.len()) {
+            write!(f, "|                                    |\n")?;
+        }
+        write!(f, "|               {}               |\n", self.category)?;
+        write!(f, "|               {}               |\n", self.name)?;
+        write!(f, "| ")?; 
+        for c in self.color.iter() {
+            write!(f, "{} ", c)?;
+        }
+        write!(f, "  ")?;
+        for t in self.types.iter() {
+            write!(f, " {} ", t)?;
+        } 
+        write!(f, "        |\n")?;
+        write!(f, "---------------------------------------\n")?;
+
+        Ok(())
+    }
 }
 
 pub type Deck = Vec<Card>;
@@ -150,20 +255,65 @@ pub enum Timing {
     OnPlay,
     WhenAttacking,
     ActivateMain,
+    CounterPhase,
 }
 
-///effects: vec![Effect::Blocker, Effect::TimedEffect(MainPhaseAction::PlayCard, EffectCost::MinusDon(2), Effect::Draw(2))]
+impl fmt::Display for Timing {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Timing::*;
+        let mut val: String = String::new();
+        match self {
+            OnPlay => val = "On Play".into(),
+            WhenAttacking => val = "When Attacking".into(),
+            ActivateMain => val = "Activate:Main".into(),
+            CounterPhase => val = "Counter".into(),
+        }
+        write!(f, "{val}")?;
+        Ok(())
+    }
+}
 
 #[derive(Debug, Clone)]
 pub enum Effect {
     Blocker,
     TimedEffect(Timing, EffectCost, Box<Effect>),
     Draw(i32),
+    OncePerTurn,
+    Rush,
+}
+
+impl fmt::Display for Effect {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Effect::*;
+        match self {
+            Blocker => write!(f, "Blocker")?,
+            TimedEffect(timing, cost, effect) => write!(f, "{} {} {}", timing, cost, effect)?,
+            Draw(i) => write!(f, "Draw {}", i)?,
+            OncePerTurn => write!(f, "Once Per Turn")?,
+            Rush => write!(f, "Rush")?,
+        }
+        Ok(())
+    }
 }
 
 #[derive(Debug, Clone)]
 pub enum EffectCost {
     MinusDon(i32),
     RestDon(i32),
-    DonAttached(Box<Card>, i32)
+    DonAttached(Box<Card>, i32),
+    Zero, // Needed for timed effects that don't require a cost, makes more sense than doing `Option<EffectCost>` everywhere.
+}
+
+impl fmt::Display for EffectCost {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use EffectCost::*;
+        match self {
+            MinusDon(n) => write!(f, "DON!! -{}", n)?,
+            RestDon(n) => write!(f, "{n}")?,
+            DonAttached(_, n) => write!(f, "DON!!x{n}")?,
+            Zero => write!(f, "")?,
+        }
+
+        Ok(())
+    }
 }
